a99179c7ee78a670a9905017b12c82d712cd4cad
abdramaa <abakar.abdraman@etu.univ-grenoble-alpes.fr>
2024-12-12 18:07:56 +0100

    adapt strategie

--- src/test/java/fr/uga/m1miage/pc/jeu/strategies/DonnantPourDeuxDonnantsAleatoireStrategieTest.java
+++ src/test/java/fr/uga/m1miage/pc/jeu/strategies/DonnantPourDeuxDonnantsAleatoireStrategieTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DonnantPourDeuxDonnantsAleatoireStrategieTest {
 
@@ -28,19 +29,38 @@ class DonnantPourDeuxDonnantsAleatoireStrategieTest {
         int coopererCount = 0;
         int trahirCount = 0;
 
-        for(int i=0; i<100; i++) {
+        for (int i = 0; i < 100; i++) {
             CoupEnum coup = strategie.getCoup(parties);
-            if(coup==CoupEnum.COOPERER) {
+            if (coup == CoupEnum.COOPERER) {
                 coopererCount++;
-            }
-            else {
+            } else if (coup == CoupEnum.TRAHIR) {
                 trahirCount++;
             }
         }
 
-        assertTrue(coopererCount > 0 || trahirCount > 0);
+        assertTrue(coopererCount > 0 || trahirCount > 0, "La stratégie ne produit aucun coup aléatoire.");
+    }
 
 
+
+    @Test
+    void testCoupAdverseRepetition() {
+
+        DonnantPourDeuxDonnantsAleatoireStrategie strategie = new DonnantPourDeuxDonnantsAleatoireStrategie();
+
+        List<PartieJoueurEntity> partiesJoueurs = new ArrayList<>();
+        PartieJoueurEntity partieJoueur1 = PartieJoueurEntity.builder().coup(CoupEnum.COOPERER).build();
+        PartieJoueurEntity partieJoueur2 = PartieJoueurEntity.builder().coup(CoupEnum.COOPERER).build();
+        partiesJoueurs.add(partieJoueur1);
+        partiesJoueurs.add(partieJoueur2);
+
+        PartieEntity partie = PartieEntity.builder().partiesJoueur(partiesJoueurs).build();
+        List<PartieEntity> parties = new ArrayList<>();
+        parties.add(partie);
+
+        CoupEnum coup = strategie.getCoup(parties);
+
+        assertThat(coup).isEqualTo(CoupEnum.COOPERER);
     }
 
 
